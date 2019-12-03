0164b2565cd4195e75bc0a8faf72da750dbf1791
chenmingjian <starvingnow@live.cn>
2019-12-03 12:32:02 +0800

    0

--- .leetcode/test.py
+++ .leetcode/test.py
@@ -1,42 +1,11 @@
-from typing import *
-import collections
+import numpy as np 
+import time
 
-class Solution:
-    def findSmallestRegion(self, regions: List[List[str]], region1: str, region2: str) -> str:
-        d = {}
-        d2 = {}
-        for i in regions:
-            # print(i)
-            d[i[0]] = {j:{} for j in i[1:]}
-        del_index = []
-        
-        for i in d:
-            for j in d:
-                if i in d[j]:
-                    d[j][i] = d[i]
-                    
-                    del_index.append(i)
-        for i in del_index:
-            d.pop(i)
-        
-        
-        deque = collections.deque([(i, d[i], None) for i in d])
-      
-        while deque:
-            k, a, father = deque.popleft()
-            
-            if father is None:
-                d2[k] = [k]
-            else:
-                d2[k] = d2[father] + [k]
-            if region1 in d2 and region2 in d2:
-                break    
-            for i in a:
-                deque.append((i,a[i], k))
+begin = time.time()
+a = [i for i in range(300000, 0, -1)]
 
-        ans = ''
-        for i, j in zip(d2[region1], d2[region2]):
-            if i == j:
-                ans = i
-            else:
-                break
+print(a[:10])
+a = np.sort(a)
+print(a[:10])
+
+print(time.time() - begin)
